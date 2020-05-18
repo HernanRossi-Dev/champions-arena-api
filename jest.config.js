69f641830a91f1baf308030d319127dfb796791c
HernanR <hernan_rossi@msn.com>
2020-05-18 11:44:47 -0700

    add ts types and update test runner

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,6 @@
-module.exports = {
-  roots: ['<rootDir>/src'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  },
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
-    }
-  },
-  coverageReporters: ['json', 'lcov', 'text', 'clover']
-}
+export default {
+  transform: {'^.+\\.ts?$': 'ts-jest'},
+  testEnvironment: 'node',
+  testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
