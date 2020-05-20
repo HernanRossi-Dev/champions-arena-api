969ebcc2f38aac70673ee1b9aead384fb1e57995
HernanR <hernan_rossi@msn.com>
2020-05-19 17:53:24 -0700

    Auth service update

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -4,10 +4,12 @@ import request from 'request'
 import { Request, Response } from 'express'
 
 const authenticate = async () => {
-  const options = { method: 'POST',
-  url: 'https://dev-qf368xa5.auth0.com/oauth/token',
-  headers: { 'content-type': 'application/json' },
-  body: '{"client_id":"D9Q8oFzgxOvpCTCFxR0mNGZJ9x6sgzq5","client_secret":"faeq5zuIcrZ4EwJskdVQ_c4tx33vsbh-qDtYqLVuC8RbmrK3CakNr8bvBRpBVc3N","audience":"champions-arena","grant_type":"client_credentials"}' }
+  const options = {
+    method: 'POST',
+    url: 'https://dev-qf368xa5.auth0.com/oauth/token',
+    headers: { 'content-type': 'application/json' },
+    body: '{"client_id":"D9Q8oFzgxOvpCTCFxR0mNGZJ9x6sgzq5","client_secret":"faeq5zuIcrZ4EwJskdVQ_c4tx33vsbh-qDtYqLVuC8RbmrK3CakNr8bvBRpBVc3N","audience":"champions-arena","grant_type":"client_credentials"}'
+  }
 
   return new Promise((resolve, reject) => {
     request(options, (error, response, body) => {
@@ -27,17 +29,16 @@ const authError = (err: Error, req: Request, res: Response) => {
 }
 
 const jwtCheck = jwt({
-    secret: jwks.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: 'https://dev-qf368xa5.auth0.com/.well-known/jwks.json'
-    }),
-    audience: 'champions-arena',
-    issuer: 'https://dev-qf368xa5.auth0.com/',
-    algorithms: ['RS256']
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://dev-qf368xa5.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'champions-arena',
+  issuer: 'https://dev-qf368xa5.auth0.com/',
+  algorithms: ['RS256']
 })
-
 
 export default {
   authenticate,
