e461bacab3e74f418da4dc8de4b2cd453c43e17c
HernanR <hernan_rossi@msn.com>
2020-05-24 17:30:32 -0700

    Create character api test framework

--- test/component/character-creation.spec.ts
+++ test/component/character-creation.spec.ts
@@ -1,133 +1,83 @@
-import mongoose from 'mongoose';
-import CreateCharacterMock from '../mocks/create-character-mock';
-import chai from 'chai';
+import { inMemoryDB, clearDB, closeDB } from '../database-helper'
+jest.mock('../../src/utils/mongo-connection', () => ({
+  getMongoConnection: jest.fn().mockImplementation(() => inMemoryDB())
+}))
+jest.mock('../../src/utils/logger', () => ({
+  error: jest.fn().mockImplementation(() => { }),
+  debug: jest.fn().mockImplementation(() => { })
+}))
+jest.mock('../../src/utils/auth-utils', () => ({
+  validateUser: jest.fn().mockImplementation(() => { return true }),
+}))
+jest.mock('../../src/utils/user-utils', () => ({
+  userDupeCheck: jest.fn().mockImplementation(() => { return false }),
+  isUser: jest.fn().mockImplementation(() => { return true }),
+}))
 
-import server from '../../src/server'
-import faker from 'faker';
+import faker from 'faker'
 import request from "supertest"
-import { ObjectID } from 'mongodb';
+import { ObjectID } from 'mongodb'
+import server from '../../src/server'
+import CreateCharacterMock from '../mocks/create-character-mock'
+import { ICharacter } from '../../src/models'
+let newCharacter: ICharacter, token: string
 
-const ObjectId = mongoose.Types.ObjectId;
+describe('Characters-API', () => {
 
-describe('Character', () => {
+  beforeAll(async () => {
+    jest.setTimeout(6000)
+    const res = await request(server).post(`/api/authenticate/`)
+      .send({ email: faker.internet.email(), password: faker.lorem.sentence() })
+    token = res.body.access_token
 
-
-  afterEach(() => {
-    server.close();
+    const characterPayload = CreateCharacterMock()
+    console.log('New HCAR: ', characterPayload)
+    const result = await request(server).post(`/api/characters`)
+      .set('authorization', 'Bearer ' + token)
+      .send({ data: characterPayload })
+    console.log('Create char result', result.body)
   })
 
-  describe('Get Characters: ', () => {
+  afterAll(async () => {
+    await clearDB()
+    await closeDB()
+    server.close()
+    jest.clearAllMocks()
+    jest.resetModules()
+  })
 
-    it('should respond status 200: /api/users', async () => {
+  describe('Get Character by id param, ', () => {
+    it('should respond status 200: /api/characters/:id', async () => {
       const id = new ObjectID()
       const res = await request(server).get(`/api/characters/${id}`)
-      const jsonRes = JSON.parse(res.text);
-      res.status.should.equal(200);
-      jsonRes.characters.should.be.a('array');
+      expect(res.status).toEqual(200)
+      expect(res.body.status).toEqual('Processed')
+      expect(res.body.data).toEqual({})
+      expect(res.body.message).toEqual(`Get character failed: ${id}`)
+      expect(res.body.errors.length).toBeGreaterThan(0)
+    })
+
+    it('should respond status 200: /api/characters/:id', async () => {
+      const id = new ObjectID()
+      const res = await request(server).get(`/api/characters/${id}`)
+      expect(res.status).toEqual(200)
+      expect(res.body.status).toEqual('Processed')
+      expect(res.body.data).toEqual({})
+      expect(res.body.message).toEqual(`Get character failed: ${id}`)
+      expect(res.body.errors.length).toBeGreaterThan(0)
     })
   })
 
-  // describe('Get Character:', () => {
-  //   it('Get character/id should respond status 422 with invalid id format', async () => {
-  //     const charId = 234234234234234242342342;
-  //     const response = await testApp.get(`/api/characters/${charId}`);
-  //     response.status.should.equal(422);
-  //   })
-
-  //   it('Get character/id should response status 404 when character not found', async () => {
-  //     const charId = new ObjectId();
-  //     const response = await testApp.get(`/api/characters/${charId}`);
-  //     response.status.should.equal(404);
-  //   })
-  // })
-
-  // describe('Create Character:', () => {
-  //   it('Create Character without a name returns status 422.', async () => {
-  //     const newCharacterNoName = CreateCharacterMock();
-  //     newCharacterNoName.name = '';
-  //     const response = await testApp.post(`/api/characters`).send(newCharacterNoName);
-
-  //     const jsonRes = JSON.parse(response.res.text);
-  //     jsonRes.message.should.equal('New Character must have a name.');
-  //     response.status.should.equal(422);
-  //   })
-
-  //   it('Create Character with valid payload should succeed status 200 and return created character.', async () => {
-  //     const newCharacter = CreateCharacterMock();
-  //     const response = await testApp.post(`/api/characters`).send(newCharacter);
-  //     const jsonRes = JSON.parse(response.res.text);
-  //     const newID = jsonRes._id;
-  //     jsonRes.should.be.a('object');
-  //     newID.should.be.a('string');
-  //     response.status.should.equal(200);
-  //   })
-  // })
-
-  // describe('Update Character:', () => {
-  //   it('Update Character with valid id and payload should succeed status 200.', async () => {
-  //     const newCharacter = CreateCharacterMock();
-  //     newCharacter.name = faker.name.firstName();
-  //     newCharacter.alignment = 'Neutral';
-  //     newCharacter.level = 1;
-  //     newCharacter.XP = 100;
-  //     const response = await testApp.post(`/api/characters`).send(newCharacter);
-  //     const jsonRes = JSON.parse(response.res.text);
-  //     response.status.should.equal(200);
-  //     testApp = chai.request(server);
-  //     await mongoose.createConnection(mongoDBUrl, { useNewUrlParser: true });
-  //     const newName = faker.name.firstName();
-  //     jsonRes.name = newName;
-  //     const newID = jsonRes._id;
-  //     jsonRes.level = 2;
-  //     jsonRes.XP = 500;
-  //     const updateResponse = await testApp.put(`/api/characters/${newID}`).send(jsonRes);
-  //     const updateJson = JSON.parse(updateResponse.res.text);
-  //     updateResponse.status.should.equal(200);
-  //     updateJson.name.should.equal(newName);
-  //     updateJson.level.should.equal(2);
-  //     updateJson.XP.should.equal(500);
-  //   })
-
-  //   it('Update Character with invalid id and payload should fail status 422.', async () => {
-  //     const newCharacter = CreateCharacterMock();
-  //     newCharacter.name = faker.name.firstName();
-  //     newCharacter.alignment = 'Neutral';
-  //     newCharacter.level = 1;
-  //     newCharacter.XP = 100;
-  //     const response = await testApp.post(`/api/characters`).send(newCharacter);
-  //     const jsonRes = JSON.parse(response.res.text);
-  //     response.status.should.equal(200);
-  //     testApp = chai.request(server);
-  //     await mongoose.createConnection(mongoDBUrl, { useNewUrlParser: true });
-  //     const newName = faker.name.firstName();
-  //     jsonRes.name = newName;
-  //     const newID = new ObjectId();
-  //     jsonRes.level = 2;
-  //     jsonRes.XP = 500;
-  //     const updateResponse = await testApp.put(`/api/characters/${newID}`).send(jsonRes);
-  //     updateResponse.status.should.equal(422);
-  //   })
-  // })
-
-  // describe('Delete Character:', () => {
-  //   it('Delete Character with valid id and payload should succeed status 200.', async () => {
-  //     const newCharacter = CreateCharacterMock();
-  //     const response = await testApp.post(`/api/characters`).send(newCharacter);
-  //     const jsonRes = JSON.parse(response.res.text);
-  //     response.status.should.equal(200);
-  //     testApp = chai.request(server);
-  //     await mongoose.createConnection(mongoDBUrl, { useNewUrlParser: true });
-  //     const newID = jsonRes._id;
-  //     const deleteResponse = await testApp.delete(`/api/characters/${newID}`);
-  //     deleteResponse.status.should.equal(200);
-  //     deleteResponse.body.status.should.equal('OK');
-  //     deleteResponse.body.result.n.should.equal(1);
-  //   })
-
-  //   it('Delete Character with invalid id should response status 422.', async () => {
-  //     const newID = new ObjectId();;
-  //     const deleteResponse = await testApp.delete(`/api/characters/${newID}`);
-  //     deleteResponse.status.should.equal(422);
-  //   })
-  // })
+  describe('Get Character by query: ', () => {
+    it('by email should response status 200 and return object', async () => {
+      const res = await request(server).get(`/api/characters`)
+        .set('authorization', 'Bearer ' + token)
+        .query({ user: newCharacter.user })
+      console.log("GEt char result:", res.body)
+      expect(res.status).toEqual(200)
+      expect(res.body.status).toEqual('Processed')
+      const userResult = res.body.data
+      expect(res.body.errors.length).toBe(0)
+    })
+  })
 })
